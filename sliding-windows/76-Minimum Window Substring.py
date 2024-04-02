e41d0f658f7198a661cd12b3684a5145a515632f
Daniel Duarte <dann.cmd@gmail.com>
2024-04-01 20:37:43 -0700

    None

--- sliding-windows/76-Minimum Window Substring.py
+++ sliding-windows/76-Minimum Window Substring.py
@@ -1,6 +1,5 @@
 from collections import Counter
 
-
 class Solution:
     def minWindow(self, s, t):
         min_sub = s
@@ -13,7 +12,7 @@ class Solution:
             sub += char
             if char in t_count:
                 count[char] += 1
-            while count > t_count or sub[0] not in t_count:
+            while count[sub[0]] > t_count[sub[0]] or sub[0] not in t_count:
                 if sub[0] in t_count and count[sub[0]] != t_count[sub[0]]:
                     count[sub[0]] -= 1
                     sub = sub[1:]
@@ -21,17 +20,17 @@ class Solution:
                     sub = sub[1:]
                 elif sub[0] in t_count and count[sub[0]] <= t_count[sub[0]] or len(sub) == 1:
                     break
-            print(sub)
             if count >= t_count:
                 if is_sub == False:
                     is_sub = True
                 min_sub = min(min_sub, sub, key=len)[:]
+
         if not is_sub:
             return ""
         else:
             return min_sub
 
-s = "ADOBECODEBANC"
-t = "ABC"
+s = "ab"
+t = "a"
 solution = Solution()
 print(solution.minWindow(s,t))
