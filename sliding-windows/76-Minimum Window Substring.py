033ed32f2e265df9edeed894a3c0ac17a2094edd
Daniel Duarte <dann.cmd@gmail.com>
2024-04-01 20:57:59 -0700

    None

--- sliding-windows/76-Minimum Window Substring.py
+++ sliding-windows/76-Minimum Window Substring.py
@@ -2,10 +2,8 @@ from collections import Counter
 
 class Solution:
     def minWindow(self, s, t):
-        min_sub = s
-        t_count = Counter(t)
-        count = Counter()
-        sub = ""
+        min_sub, sub = s, ""
+        t_count, count = Counter(t), Counter()
         is_sub = False
 
         for char in s:
@@ -13,18 +11,17 @@ class Solution:
             if char in t_count:
                 count[char] += 1
             while count[sub[0]] > t_count[sub[0]] or sub[0] not in t_count:
-                if sub[0] in t_count and count[sub[0]] != t_count[sub[0]]:
+                if sub[0] in t_count and count[sub[0]] <= t_count[sub[0]] or len(sub) == 1:
+                    break
+                elif sub[0] in t_count and count[sub[0]] != t_count[sub[0]]:
                     count[sub[0]] -= 1
                     sub = sub[1:]
                 elif len(sub) > 1 and sub[0] not in t_count:
                     sub = sub[1:]
-                elif sub[0] in t_count and count[sub[0]] <= t_count[sub[0]] or len(sub) == 1:
-                    break
             if is_sub == True:
                 min_sub = min(min_sub, sub, key=len)[:]
-            elif is_sub == False and count >= t_count:
-                if is_sub == False:
-                    is_sub = True
+            elif is_sub == False and len(sub) >= len(t) and count >= t_count:
+                is_sub = True
                 min_sub = min(min_sub, sub, key=len)[:]
 
         if not is_sub:
