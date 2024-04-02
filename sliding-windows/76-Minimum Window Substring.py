30da93eaa0d309399787bb95c8b7ad80ad627998
Daniel Duarte <dann.cmd@gmail.com>
2024-04-01 20:28:15 -0700

    None

--- sliding-windows/76-Minimum Window Substring.py
+++ sliding-windows/76-Minimum Window Substring.py
@@ -2,50 +2,36 @@ from collections import Counter
 
 
 class Solution:
-    def minWindow(self, s,t):
-        t_len = len(t)
-        l = 0
-        r = t_len - 1
-        t_count = Counter(list(t))
-        t_set = set(list(t))
-        t_len = len(t)
+    def minWindow(self, s, t):
+        min_sub = s
+        t_count = Counter(t)
         count = Counter()
-        total_length = len(s)
-        minWindow = ""
+        sub = ""
+        is_sub = False
 
-        if total_length >= t_len:
-            #start count
-            for i in range(t_len):
-                if s[i] in t_set:
-                    count[s[i]] += 1
-            if count == t_count:
-                minWindow = s[l:r+1]
-                return minWindow
-
-            # Check the loop statement
-            while r < total_length - 1 and l <= r:
-                while count != t_count and l <= r:
-                    r += 1
-                    if s[r] in t_set:
-                        count[s[r]] += 1
-                        while count[s[r]] > t_count[s[r]] and l < r:
-                            if s[l] in t_set:
-                                count[s[l]] -= 1
-                            l += 1
-                    if r == total_length - 1:
-                        break
-                while count == t_count and l <= r:
-                    if count == t_count and (len(minWindow) > len(s[l:r+1]) or minWindow == ""):
-                        minWindow = s[l:r+1]
-                    if s[l] in t_set:
-                        count[s[l]] -= 1
-                    l += 1
-
-            return minWindow
-        else:
+        for char in s:
+            sub += char
+            if char in t_count:
+                count[char] += 1
+            while count > t_count or sub[0] not in t_count:
+                if sub[0] in t_count and count[sub[0]] != t_count[sub[0]]:
+                    count[sub[0]] -= 1
+                    sub = sub[1:]
+                elif len(sub) > 1 and sub[0] not in t_count:
+                    sub = sub[1:]
+                elif sub[0] in t_count and count[sub[0]] <= t_count[sub[0]] or len(sub) == 1:
+                    break
+            print(sub)
+            if count >= t_count:
+                if is_sub == False:
+                    is_sub = True
+                min_sub = min(min_sub, sub, key=len)[:]
+        if not is_sub:
             return ""
+        else:
+            return min_sub
 
-s = "bba"
-t = "ab"
+s = "ADOBECODEBANC"
+t = "ABC"
 solution = Solution()
-print(solution.minWindow(s,t))
+print(solution.minWindow2(s,t))
